8a149f74546ab890536655f1824375b0cb0c7019
Anushka Nagdeote <anushka4619@gmail.com>
2023-11-27 15:37:08 +0530

    Inverted Triangle Pattern (Readable and Maintainable)

--- practicals/inverted_triangle.php
+++ practicals/inverted_triangle.php
@@ -1,8 +1,10 @@
 <?php
 $rows = 5; // Change this value to create a different-sized inverted triangle
 
-for ($i = $rows; $i >= 1; $i--) {
-    for ($j = 1; $j <= $i; $j++) {
+for ($i = $rows; $i >= 1; $i--) // for rows
+{
+    for ($j = 1; $j <= $i; $j++) // for columns
+    {
         echo "* ";
     }
     echo "\n";
