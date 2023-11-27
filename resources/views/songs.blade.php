ee90942c7f46b10c152c87d19cf46cd120c016d4
Anushka Nagdeote <anushka4619@gmail.com>
2023-11-28 02:01:08 +0530

    component is created

--- resources/views/songs.blade.php
+++ resources/views/songs.blade.php
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-    <tr>
-    @foreach ($songs as $song)
-<tr>
-<td>{{ $song->getTitle() }}</td>
-<td>{{ $song->getArtist() }}</td>
-</tr>
-@endforeach
-    </tr>
 
-</body>
-</html>
+<x-layouts.main>
+</x-layouts.main>
