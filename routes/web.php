13ba1ae8513cc87838f961343150ceca54e50f61
Anushka Nagdeote <anushka4619@gmail.com>
2023-11-28 02:20:46 +0530

    Added external CSS

--- routes/web.php
+++ routes/web.php
@@ -20,31 +20,33 @@ Route::get('/veggies', function () {
     return view('table');
 });
 Route::get('/songs_static', function () {
-    $song1 = new Song();
-    $song1->setTitle("Stan");
-    $song1->setArtist("Eminem");
     // $song1 = new Song();
+
+
+    return view('songs_static'); 
+  });
+
+
+  Route::get('/songs', function () {
+// $song1 = new Song();
     // $song1->setTitle("Stan");
     // $song1->setArtist("Eminem");
 
-    $song2 = new Song();
-    $song2->setTitle("Nothing Else Matters");
-    $song2->setArtist("Metallica");
     // $song2 = new Song();
     // $song2->setTitle("Nothing Else Matters");
     // $song2->setArtist("Metallica");
-
-    $song3 = new Song();
-    $song3->setTitle("With You");
-    $song3->setArtist("A P Dhillon");
+  
     // $song3 = new Song();
     // $song3->setTitle("With You");
     // $song3->setArtist("A P Dhillon");
 
-    return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]); 
     return view('songs_static'); 
   });
 
+
+  Route::get('/songs', function () {
+    return view('songs', [ 'songs' => Song::all() ] );
+});
 
   Route::get('/songs', function () {
     return view('songs', [ 'songs' => Song::all() ] );
