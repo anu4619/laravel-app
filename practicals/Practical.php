2d77e25dab6fe319fdd507bf6d364c75d45a9d34
Anushka Nagdeote <anushka4619@gmail.com>
2023-11-27 15:56:00 +0530

    Add unit test case for Practical class

--- practicals/Practical.php
+++ practicals/Practical.php
@@ -1,19 +1,19 @@
 <?php
 
+namespace practicals;
+
 class Practical {
     //Prints "Hello, World!" to the standard output.
     public static function printHello() {
         echo "Hello, World!";
     }
-
-
+ 
     //Takes two numbers as arguments and returns their sum.
-
+     
     public static function add($num1, $num2) {
         return $num1 + $num2;
     }
-
-
+    
      //Takes an integer 'n' as an argument and returns an array of
     public static function generateFibonacciSequence($n) {
         $fibSequence = array();
---
+++ practicals/numver_swapping.php
@@ -0,0 +1,13 @@
+<?php
+$a = 5;
+$b = 10;
+
+echo "Before swapping: a = $a, b = $b\n";
+
+// Swapping logic
+$temp = $a;
+$a = $b;
+$b = $temp;
+
+echo "After swapping: a = $a, b = $b\n";
+?>
