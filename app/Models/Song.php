370c0edc96fc82d202df106633fef4e85a9b8bb7
Anushka Nagdeote <anushka4619@gmail.com>
2023-11-27 23:45:21 +0530

    Create Song database model

---
+++ app/Models/Song.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+class Song extends Model
+{
+    use HasFactory;
+    // Add fillable fields corresponds to database columns
+    protected $fillable=['title', 'artist'];
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    public function getArtist() {
+        return $this->artist;
+    }
+
+
+    public function setArtist($artist) {
+        $this->artist = $artist;
+    }
+
+    public function getGenre() {
+        return $this->genre;
+    }
+
+
+    public function setGenre($genre) {
+        $this->genre = $genre;
+    }
+
+    public function getTempo() {
+        return $this->tempo;
+    }
+
+
+    public function setTempo($tempo) {
+        if (!is_numeric($tempo) || is_float($tempo)) {
+            throw new \InvalidArgumentException("inputs must be numeric.");
+        }
+        $this->tempo = $tempo;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,47 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+use App\Models\Song;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/veggies/{sabji}', function ($sabji) {
-   
     return $sabji;
 })->whereIn('sabji', ['baigan', 'bhindi', 'aaloo', 'gobhi']);
+
 Route::get('/greeting', function () {
     return 'Hello World';
 });
+
 Route::get('/hello', function () {
     return view('veggies');
 });
 
-// On route '/veggies 'a chart should be seen , with name and price of veggies
+// On route '/veggies,' a chart should be seen, with the name and price of veggies
 Route::get('/veggies', function () {
     return view('table');
 });
 
+// Static songs route
+Route::get('/songs_static', function () {
+    $song1 = new Song();
+    $song1->setTitle("Stan");
+    $song1->setArtist("Eminem");
+
+    $song2 = new Song();
+    $song2->setTitle("Nothing Else Matters");
+    $song2->setArtist("Metallica");
+
+    $song3 = new Song();
+    $song3->setTitle("With You");
+    $song3->setArtist("A P Dhillon");
+
+    return view('songs', ['songs' => [$song1, $song2, $song3]]);
+});
+
+// Dynamic songs route
+Route::get('/songs', function () {
+    return view('songs', ['songs' => Song::all()]);
+});
